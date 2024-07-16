fb6e6ef9652acdd8047a4ecf3df3548dab1b7a5e
Sreerag Rajesh Babu <Sreerag.Babu@asl.mum.sst>
2024-07-16 12:17:56 +0530

    innit

---
+++ Models/Player.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerApp.Models
+{
+    internal class Player
+    {
+        static int DEFAULT_AGE = 18;
+        
+        public int Id { get; }
+        public string Name { get; }
+        public int Age { get; }
+
+        public Player(int id, string name) 
+        {
+            Id = id;
+            Name = name;
+            Age = DEFAULT_AGE;
+        }
+
+        public Player(int id, string name, int age) : this(id, name) 
+        { 
+            Age = age;
+        }
+
+        public static Player WhoIsElder(Player[] players)
+        {
+            int MaxAge = 0;
+            Player playerWithMaxAge = null;
+            foreach (Player player in players) {
+                if (player.Age > MaxAge) { 
+                    MaxAge = player.Age;
+                    playerWithMaxAge = player;
+                }
+            }
+            return playerWithMaxAge;
+        }
+
+        public override string ToString()
+        {
+            Console.WriteLine("==========MAX AGE=========");
+            return $"Player id: {Id} \n" +
+                $"Player Name: {Name} \n" +
+                $"Player age: {Age} \n";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using PlayerApp.Models;
+namespace PlayerApp
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player[] players = {
+                new Player(101, "Ramesh", 23),
+                new Player(102, "Suresh", 13),
+                new Player(103, "Ajesh"),
+                new Player(104, "Rakesh", 44),
+                new Player(105, "Mahesh", 21),
+            };
+
+            Player maxAgePlayer = Player.WhoIsElder(players);
+            Console.WriteLine($"Player with maximum age is {maxAgePlayer}");
+        }
+
+    }
+}
